f456b0fa7a3a101ea041c698b2154679eadfa3ec
hosamghazal <hosam.ghazal@ejust.edu.eg>
2026-04-02 16:56:12 +0200

    predicting the admission status based of several variables
    Added summary of the model to the analysis script.

--- admission_analysis.R
+++ admission_analysis.R
@@ -8,7 +8,6 @@ num_train_obs <- round(split_ratio * total_observations)
 train_indices <- sample(1:total_observations, num_train_obs)
 training_set <- admission[train_indices, ]
 test_set <- admission[-train_indices, ]
-
 cat("Number of observations in the training set:", nrow(training_set), "\n")
 
 model1 <- glm(Chance.of.Admit ~ ., data = training_set)
